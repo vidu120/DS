d70731dc62650e4d5903370642c50dad9ebf6f11
vidu120 <vidhangoyal10@gmail.com>
2020-10-04 20:03:48 +0530

    Inheritance example for stack

--- DataStructures1/week1_basic_data_structures/mysol/stack_with_max/main.cpp
+++ DataStructures1/week1_basic_data_structures/mysol/stack_with_max/main.cpp
@@ -1,85 +1,37 @@
 #include<iostream>
+#include<stack>
 
 using namespace std;
 
-//our node class a
-class Node{
-    public:
-        int data;
-        Node *next;
-        Node();
-        Node(int in);
-        ~Node();
-};
-//constructors for our Node class
-Node::Node(){
-    next = nullptr;
-}
-Node::Node(int in){
-    data = in;
-    next = nullptr;
-}
 
-Node::~Node(){}
 
-class stackWithMax{
-    private:
-        Node *headMaxList;
+//new stack with max element functionality inherited from the stack<int> class
+class maxImplementedStack : public stack<int>{
     public:
-        //a different stack to keep track of the max value as we add elements in the stack
-        stackWithMax();
-        ~stackWithMax();
-        void push(int data);
-        bool empty();
-        int pop();
-        int max(); 
+        int Max();
+        void Push(int data);
+        void Pop();
 };
 
-stackWithMax::stackWithMax(){
-     headMaxList = nullptr;
+int maxImplementedStack::Max(){
+    return this->top();
+}
+void maxImplementedStack::Pop(){
+    this->pop();
 }
 
-stackWithMax::~stackWithMax(){}
-
-void stackWithMax::push(int data){
-    if (headMaxList == nullptr){
-        headMaxList->data = data;
-        headMaxList->next = nullptr;
-        return;
-    }
-    //for keeping track of the max element stacks
-    Node maxElem;
-    if(headMaxList->data < data){
-        maxElem = Node(data);   
+void maxImplementedStack::Push(int data){
+    if(this->empty()){
+        this->push(data);
     }else{
-        maxElem  = Node(headMaxList->data);
+        if (this->top() < data){
+            this->push(data);
+        }else{
+            this->push(this->top());
+        }
     }
-    maxElem.next = headMaxList;
-    headMaxList = &maxElem;
 }
 
-bool stackWithMax::empty(){
-    if(headMaxList == nullptr){
-        return true;
-    }
-    return false;
-}
-
-int stackWithMax::pop(){
-    if(headMaxList == nullptr){
-        cout << "Error - No elements in the stack"<< endl;
-        return 0;
-    }
-    Node *top = headMaxList;
-    headMaxList = headMaxList->next;
-    int popped = top->data;
-    free(top);
-    return popped;
-}
-
-int stackWithMax::max(){
-    return pop();
-}
 
 
 int main(){
@@ -89,21 +41,18 @@ int main(){
     string queryType;
     int data;
 
-    stackWithMax max = stackWithMax();
+    //this is the stack of the max elements
+    maxImplementedStack maxElemList;
 
-    // while (queries--)
-    // {   
-    //     cin >> queryType;
-    //     if (queryType == "push"){
-    //         cin >> data;
-    //         max.push(data);
-    //     }else if(queryType == "top"){
-    //         max.pop();
-    //     }else{
-    //         cout << max.max() << endl;
-    //     }
-    // }
-    max.push(1);
-    cout << max.max() << endl;
-    
+    while(queries--){
+        cin >> queryType;
+        if(queryType == "push"){
+            cin >> data;
+            maxElemList.Push(data);
+        }else if(queryType == "pop"){
+            maxElemList.Pop();
+        }else{
+            cout << maxElemList.Max() << endl;
+        }
+    }
 }
